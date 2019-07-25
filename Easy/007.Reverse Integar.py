f87254d15dd0aa8e08e59e4199e311d94ec8d2d5
tailer954 <44743933+tailer954@users.noreply.github.com>
2019-07-25 20:13:28 +0800

    Update 007.Reverse Integar.py

--- Easy/007.Reverse Integar.py
+++ Easy/007.Reverse Integar.py
@@ -1,7 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-import numpy
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        if x >= 2**31-1 or x <= -2**31: return 0
+        else:
+            strX = str(x)               #change x to the str form so that I can operate x easily
+            
+        if x >= 0 :
+            revst = strX[::-1]          #This is the key point
+        else:
+            temp = strX[1:] 
+            temp2 = temp[::-1] 
+            revst = "-" + temp2
+            
+        if int(revst) >= 2**31-1 or int(revst) <= -2**31: return 0
+        else: return int(revst)
