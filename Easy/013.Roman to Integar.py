6fe763fe0c3d8b88a9dd5399ad691ec51084e6d0
Yi-Tse-Wu <44743933+tailer954@users.noreply.github.com>
2019-08-02 19:09:35 +0800

    Update 013.Roman to Integar.py

--- Easy/013.Roman to Integar.py
+++ Easy/013.Roman to Integar.py
@@ -1,7 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-import numpy
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        RomanSymbol = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        ans = 0
+        for i in range(len(s)):
+            ans = ans + RomanSymbol[s[i]]
+            if i >= 1 and RomanSymbol[s[i]] > RomanSymbol[s[i-1]]:
+                ans = ans - RomanSymbol[s[i-1]]*2
+        return ans
