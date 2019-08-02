dac6a8207bd18e2a2d5ae1250bb04dc44092b7e5
Yi-Tse-Wu <44743933+tailer954@users.noreply.github.com>
2019-08-02 19:13:22 +0800

    Update 013.Roman to Integar.py

--- Easy/013.Roman to Integar.py
+++ Easy/013.Roman to Integar.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        RomanSymbol = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        RomanSymbol = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000} # key point : build a dictionary.
         ans = 0
         for i in range(len(s)):
             ans = ans + RomanSymbol[s[i]]
