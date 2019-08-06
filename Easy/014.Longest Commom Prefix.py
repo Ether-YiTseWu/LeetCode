e210ff605e8a7a6ba413eb11127d2c3cd00afa95
Yi Tse Wu <44743933+tailer954@users.noreply.github.com>
2019-08-06 15:39:14 +0800

    Update 014.Longest Commom Prefix.py

--- Easy/014.Longest Commom Prefix.py
+++ Easy/014.Longest Commom Prefix.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-import numpy
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        smallLen = 100
+        for word in strs:
+            temp = len(word)
+            if temp < smallLen:
+                smallLen = temp                      # acquire the most shortest string length in strs
+                
+        sameNum = 0
+        for i in range(smallLen):
+            tempCount = 0
+            for word in strs:
+                if word[i] == strs[0][i]:
+                    tempCount = tempCount + 1
+            if tempCount != len(strs): break
+            if tempCount == len(strs):
+                sameNum = sameNum + 1                # compute the number of same letters in strs
+                
+        if len(strs) == 0 :
+            return('')
+        else:
+            return strs[0][0:sameNum]                # output
+                
