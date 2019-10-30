e00c9a5fd32771498f33cc53157df4d483eeec85
Yi Tse Wu <44743933+tailer954@users.noreply.github.com>
2019-10-30 16:27:35 +0800

    Update 202.Happy number.py

--- Easy/202.Happy number.py
+++ Easy/202.Happy number.py
@@ -1,7 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-import numpy
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        nStr = str(n)
+        checkList = []
+        while(1):
+            sumNum = 0
+            for i in range(0,len(nStr)):
+                sumNum = sumNum + int(nStr[i])*int(nStr[i])
+            if sumNum == 1:
+                return 1 
+            elif sumNum in checkList:
+                return 0
+            checkList.append(sumNum);
+            nStr = str(sumNum)
